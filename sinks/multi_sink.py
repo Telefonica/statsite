d376b97fcaa77dfaf5a0db2cde0a2cab0f1ca610
javierm <javier.munozferrara@telefonica.com>
2021-03-11 10:38:53 +0100

    Print sink output if any to view errors

--- sinks/multi_sink.py
+++ sinks/multi_sink.py
@@ -9,14 +9,20 @@ class Sinks:
     def __init__(self, sink_commands):
         self.sinks = []
         self.lines_sent = 0
-        for param in sink_commands:
-            print(f'Executing: {param}')
+        for idx, param in enumerate(sink_commands):
+            print(f'Executing {idx + 1}: {param}')
             self.sinks.append(subprocess.Popen(param.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
 
     def write_lines(self, lines):
         self.lines_sent += len(lines)
         encoded_lines = [line.encode('utf-8') for line in lines]
         [sink.stdin.writelines(encoded_lines) for sink in self.sinks]
+
+    def print_errors(self):
+        for idx, sink in enumerate(self.sinks):
+            (out, _) = sink.communicate()
+            if out:
+                print(f'Error in {idx + 1}: {out}')
 
 
 sinks = Sinks(sys.argv[1:])
@@ -25,5 +31,7 @@ input_lines = sys.stdin.readlines(MAX_READLINES)
 while input_lines:
     sinks.write_lines(input_lines)
     input_lines = sys.stdin.readlines(MAX_READLINES)
+
+sinks.print_errors()
 
 print(f"{sinks.lines_sent} lines sent")
